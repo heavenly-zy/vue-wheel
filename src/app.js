b49d239c97f9db948a5ab2ef94e0783cace1e1e3
heavenly-zy <1457715339@qq.com>
2019-09-21 23:12:43 +0800

    使用 chai.expect 添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -15,3 +15,72 @@ new Vue({
         loading3:false
     }
 })
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+{
+    console.log(Button) // 由打印结果可知button是一个对象
+    const Constructor = Vue.extend(Button) // 把Button组件变成一个构造函数
+    console.log(Constructor) // 函数
+    const vm = new Constructor({ // vm(ViewModel)是一个Vue实例，这里是Button的一个实例对象
+        propsData:{
+            icon:'settings' // 设置icon的值为'settings'
+        }
+    }) // 实例化构造函数
+    vm.$mount('#test') // 将实例挂载到id为test的标签上
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings') // 我期望use的href是'#i-settings'
+    vm.$el.remove() // 测试用例通过就把这个vm元素移除
+    vm.$destroy() // 删除vm对象
+}
+{
+    const Constructor = Vue.extend(Button) 
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            loading:true
+        }
+    }) 
+    vm.$mount() 
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading') // 我期望use的href是'#i-settings'
+    vm.$el.remove() 
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button) 
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings'
+        }
+    }) 
+    vm.$mount(div) 
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1') 
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button) 
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+        }
+    }) 
+    vm.$mount(div) 
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2') 
+    vm.$el.remove()
+    vm.$destroy()
+}
