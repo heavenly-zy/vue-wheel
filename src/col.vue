8276ebd55854f0f927ffc6800da26161cde0ebfb
heavenly-zy <1457715339@qq.com>
2019-09-25 00:06:49 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
+    <div style="border:1px solid green;height:100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    span: [Number, String]
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {
+      gutter: 0
+    };
+  },
+  created() {
+    console.log("col created");
+  },
+  mounted() {
+    console.log("col mounted");
   }
 };
 </script>
@@ -15,14 +37,18 @@ export default {
 <style lang="scss" scoped>
 .col {
   height: 100px;
-  background: gray;
   width: 50%;
-  border: 1px solid red;
 
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  name: "WheelRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  created() {
+    console.log("row created");
+  },
+  mounted() {
+    console.log("row mounted");
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+    console.log(this.$children);
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .row {
   display: flex;
