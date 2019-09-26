9766e7e6037b9cdda452c32c80c1cc072f494988
heavenly-zy <1457715339@qq.com>
2019-09-26 23:31:25 +0800

    默认布局基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'WheelSider'
+}
+</script>
+
+<style lang="scss" scoped>
+.sider {
+}
+</style>
